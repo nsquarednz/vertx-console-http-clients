03c31a3ff2016c9f75fa5a5e102bb6f078d3a306
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-08-05 12:00:51 -0400

    Card styles

--- frontend/src/HttpClientCard.vue
+++ frontend/src/HttpClientCard.vue
@@ -1,5 +1,11 @@
+<style lang="scss" scoped>
+.http-client {
+    margin: 15px;
+}
+</style>
+
 <template>
-    <div class="http-client">
+    <div class="http-client card-pf">
         <h2>{{ name }}</h2>
         {{ httpClientsMetrics }}
     </div>
