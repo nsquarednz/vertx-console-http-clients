6e03fe2ae6d59e530fd4d2f86a2c4fac1297842a
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-08-05 11:05:44 -0400

    Test getting full client list

--- frontend/src/HttpClientsPage.vue
+++ frontend/src/HttpClientsPage.vue
@@ -1,10 +1,11 @@
 <template>
-    <div class="httpClients">
-        {{ httpClientsMetrics }}
-    </div>
+    <pre class='httpClients'>{{ clientNames }}</pre>
 </template>
 
 <script>
+const prefix = 'vertx.http.clients.';
+const defaultClientName = prefix.substring(0, prefix.length - 1);
+
 export default {
     name: 'HTTP Clients',
     data() {
@@ -12,11 +13,25 @@ export default {
             httpClientsMetrics: {}
         }
     },
+    computed: {
+        clientNames() {
+            const suffix = '.connections';
+            return [ defaultClientName ].concat(Object.keys(this.httpClientsMetrics)
+                .filter(el => el.startsWith(prefix))
+                .map(el => el.substring(prefix.length))
+                .filter(el => el.endsWith(suffix))
+                .map(el => el.substring(0, el.length - suffix.length))
+                .map(el => prefix + el));
+        }
+    },
     beforeMount() {
         const updateData = () => {
             this.$http.get(window.location.pathname + '/httpclients')
                 .then(response => response.data)
-                .then(data => this.httpClientsMetrics = data);
+                .then(data => {
+                    data['vertx.http.clients.fake-client.connections'] = true;
+                    this.httpClientsMetrics = data
+                });
         };
         updateData();
         this.updateTask = setInterval(updateData, 1000);
