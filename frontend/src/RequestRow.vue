d3045a160dad0bd054abc4cc29e8bc197dc412e8
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-08-06 21:40:31 -0400

    Abbreviate rates in rows

--- frontend/src/RequestRow.vue
+++ frontend/src/RequestRow.vue
@@ -1,20 +1,25 @@
 <template>
     <tr v-if="requestData">
         <th class="type bold">{{ type }}</th>
-        <th class="rate">{{ requestData.oneSecondRate.toFixed(2) }}/s</th>
-        <th class="m-1">{{ requestData.oneMinuteRate.toFixed(2) }}/s</th>
-        <th class="m-5">{{ requestData.fiveMinuteRate.toFixed(2) }}/s</th>
-        <th class="m-15">{{ requestData.fifteenMinuteRate.toFixed(2) }}/s</th>
+        <th class="rate">{{ abbreviate(requestData.oneSecondRate.toFixed(2), 2) }}/s</th>
+        <th class="m-1">{{ abbreviate(requestData.oneMinuteRate.toFixed(2), 2) }}/s</th>
+        <th class="m-5">{{ abbreviate(requestData.fiveMinuteRate.toFixed(2), 2) }}/s</th>
+        <th class="m-15">{{ abbreviate(requestData.fifteenMinuteRate.toFixed(2), 2) }}/s</th>
     </tr>
     <tr v-else/>
 </template>
 
 <script>
+import abbreviate from 'number-abbreviate';
+
 export default {
     props: {
         type: String,
         getMetric: Function,
         keyName: String
+    },
+    beforeMount() {
+        this.abbreviate = abbreviate;
     },
     computed: {
         requestData() {
